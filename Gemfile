6c05aaebce98033e0e2fe6f940222773dd7a9715
Cronos <lexsandr46@gmail.com>
2015-07-29 20:30:10 +0300

    Finish static pages

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 group :test do
   gem 'selenium-webdriver'
   gem 'capybara'
+  gem 'database_cleaner'
 end
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+ describe "Static pages" do
+
+  describe "Home page" do
+    it "should have the content 'Communicatron'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Communicatron')
+    end
+    
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Communicatron | Home")
+    end
+  end
+  
+  describe "Help page" do
+    it "should have the content 'Help'" do
+       visit '/static_pages/help'
+       expect(page).to have_content('Help')
+    end
+    
+    it "should have the title 'Help'" do
+        visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Communicatron | Help")
+    end
+  end
+  
+  describe "About page" do
+    it "should have the content 'About'" do
+       visit '/static_pages/about'
+       expect(page).to have_content('About')
+    end
+    
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Communicatron | About")
+    end
+  end
+ 
+end
