fd5c97a51527d4f80fa28246cc7446a7fb5c9ceb
Cronos <lexsandr46@gmail.com>
2015-08-01 13:45:02 +0300

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,40 +1,43 @@
 require 'rails_helper'
  describe "Static pages" do
+  subject { page }
 
   describe "Home page" do
-    it "should have the content 'Communicatron'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Communicatron')
-    end
-    
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Communicatron | Home")
-    end
+    before { visit root_path }
+
+    it { should have_content('Communicatron') }
+    it { should have_title("Ruby on Rails Tutorial Communicatron") }
+    it { should_not have_title('| Home') }
   end
   
   describe "Help page" do
-    it "should have the content 'Help'" do
-       visit '/static_pages/help'
-       expect(page).to have_content('Help')
-    end
-    
-    it "should have the title 'Help'" do
-        visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Communicatron | Help")
-    end
+      before { visit help_path}
+      
+      it { should have_content('Help') }
+      it { should have_title("Ruby on Rails Tutorial Help") }
+      it { should_not have_title('| Help') }
   end
   
   describe "About page" do
-    it "should have the content 'About'" do
-       visit '/static_pages/about'
-       expect(page).to have_content('About')
-    end
-    
-    it "should have the title 'About'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Communicatron | About")
-    end
+      before { visit about_path}
+      
+      it { should have_content('About') }
+      it { should have_title("Ruby on Rails Tutorial About") }
+      it { should_not have_title('| About') }
   end
- 
+     
+  describe "Contact page" do
+      before { visit contact_path}
+      
+      it { should have_content('Contact') }
+      it { should have_title("Ruby on Rails Tutorial Contact") }
+      it { should_not have_title('| Contact') }
+  end
+     
+   describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
 end
