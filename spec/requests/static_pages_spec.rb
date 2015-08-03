09f7b406520b6cf1fe6ba5ecc59e7eea98edd08b
Cronos <lexsandr46@gmail.com>
2015-08-03 15:09:29 +0300

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,37 +7,32 @@ require 'rails_helper'
 
     it { should have_content('Communicatron') }
     it { should have_title("Ruby on Rails Tutorial Communicatron") }
-    it { should_not have_title('| Home') }
+    
   end
   
   describe "Help page" do
       before { visit help_path}
       
       it { should have_content('Help') }
-      it { should have_title("Ruby on Rails Tutorial Help") }
-      it { should_not have_title('| Help') }
+      it { should have_title("Ruby on Rails Tutorial Communicatron | Help") }
+    
   end
   
   describe "About page" do
       before { visit about_path}
       
       it { should have_content('About') }
-      it { should have_title("Ruby on Rails Tutorial About") }
-      it { should_not have_title('| About') }
+      it { should have_title("Ruby on Rails Tutorial Communicatron | About") }
+    
   end
      
   describe "Contact page" do
       before { visit contact_path}
       
       it { should have_content('Contact') }
-      it { should have_title("Ruby on Rails Tutorial Contact") }
-      it { should_not have_title('| Contact') }
+      it { should have_title("Ruby on Rails Tutorial Communicatron | Contact") }
+    
   end
      
-   describe "signup page" do
-    before { visit signup_path }
-
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
-  end
+   
 end
